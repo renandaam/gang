8eec443b79a208fc65de694b03def0988bb3b554
renandaam <renandamelatiputri999@gmail.com>
2020-08-05 20:13:30 +0700

    menambahkan file di github

---
+++ tes.js
@@ -0,0 +1,1 @@
+alert("YOK BISA YOK!");
---
+++ try.js
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title>YOK SEMANGAT MAGANG YOK!</title>
+    </head>
+    <body>
+
+    </body>
+</html>
